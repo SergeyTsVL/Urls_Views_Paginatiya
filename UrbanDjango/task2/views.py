fe79af282e85fb7fa8223befea5b3c8286dcdd32
tsars <tsarskiytsarskiy@mail.ru>
2024-08-09 15:39:11 +0700

    pagination

--- UrbanDjango/task2/views.py
+++ UrbanDjango/task2/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
+from .models import Post
 
 def index(request):
-    context = {
-        'products': ['Товар 1', 'Товар 2', 'Товар 3'],
-    }
-    return render(request, 'second_task/class_template.html', context)
+    posts = Post.objects.all().order_by('-created_at')
+    paginator = Paginator(posts, 3)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'second_task/class_template.html', {'page_obj': page_obj})
