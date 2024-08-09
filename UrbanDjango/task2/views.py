4ce299e24ffb8e3df7fadb33085a45cf9f8501cd
tsars <tsarskiytsarskiy@mail.ru>
2024-08-09 13:15:35 +0700

    18 модуль Urls и Views.

---
+++ UrbanDjango/task2/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+def index(request):
+    context = {
+        'products': ['Товар 1', 'Товар 2', 'Товар 3'],
+    }
+    return render(request, 'second_task/class_template.html', context)
